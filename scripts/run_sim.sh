bd5a733e84e8baec59a579e6ec46ddded29fd1e2
PonomarevDA <ponomarevda96@gmail.com>
2024-06-07 20:06:53 +0300

    rename sitl commands

--- scripts/run_sim.sh
+++ scripts/run_sim.sh
@@ -19,10 +19,8 @@ Commands:
   px4_v1_15_0_cyphal_octoplane_vtol       Run dynamics simulator in Cyphal HITL mode for vtol 8 motors airframe.
   cyphal_and_dronecan_inno_vtol           Run dynamics simulator in DroneCan + Cyphal mode for vtol airframe.
                                           This mode uses 2 serial ports and is in the alpha testing stage yet.
-  sitl_inno_vtol                          Run dynamics simulator in MAVLink SITL mode for vtol airframe
-  sitl_flight_goggles                     Run dynamics simulator in MAVLink SITL mode for flight_goggles airframe
-  sitl_inno_vtol_with_flight_stack        Run dynamics simulator in MAVLink SITL mode for vtol airframe (with additional including px4.launch)
-  sitl_flight_goggles_with_flight_stack   Run dynamics simulator in MAVLink SITL mode for flight_goggles airframe (with additional including px4.launch)
+  px4_v1_12_0_mavlink_quadplane_vtol      Run dynamics simulator in MAVLink SITL mode for vtol airframe
+  px4_v1_12_0_mavlink_quadcopter          Run dynamics simulator in MAVLink SITL mode for flight_goggles airframe
 
 Auxilliary commands:
   ros                                     Source ROS and catkin_ws setup.bash files
@@ -41,12 +39,6 @@ setup_ros() {
         source /opt/ros/$ROS_DISTRO/setup.bash
         source $MOST_PROBABLE_CATKIN_WS_SETUP_BASH_PATH
     fi
-}
-
-setup_sitl_px4_flight_stack() {
-    source ~/PX4-Autopilot/Tools/setup_gazebo.bash ~/PX4-Autopilot ~/PX4-Autopilot/build/px4_sitl_default
-    export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/PX4-Autopilot
-    export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/PX4-Autopilot/Tools/sitl_gazebo
 }
 
 setup_dronecan_hitl() {
@@ -181,7 +173,7 @@ cyphal_and_dronecan_inno_vtol() {
         dynamics:=quadcopter
 }
 
-sitl_inno_vtol() {
+px4_v1_12_0_mavlink_quadplane_vtol() {
     setup_ros
     roslaunch innopolis_vtol_dynamics sitl.launch   \
         logging_type:=standard_vtol                 \
@@ -192,7 +184,7 @@ sitl_inno_vtol() {
         run_sitl_flight_stack:="false"
 }
 
-sitl_flight_goggles() {
+px4_v1_12_0_mavlink_quadcopter() {
     setup_ros
     roslaunch innopolis_vtol_dynamics sitl.launch   \
         logging_type:=quadcopter                    \
@@ -201,30 +193,6 @@ sitl_flight_goggles() {
         mixer:=direct_mixer                         \
         dynamics:=quadcopter                        \
         run_sitl_flight_stack:="false"
-}
-
-sitl_inno_vtol_with_flight_stack() {
-    setup_ros
-    setup_sitl_px4_flight_stack
-    roslaunch innopolis_vtol_dynamics sitl.launch   \
-        logging_type:=standard_vtol                 \
-        sitl_vehicle:=innopolis_vtol                \
-        vehicle_params:=quadrotor                   \
-        mixer:=vtol_13070_mixer                     \
-        dynamics:=vtol_dynamics                     \
-        run_sitl_flight_stack:="true"
-}
-
-sitl_flight_goggles_with_flight_stack() {
-    setup_ros
-    setup_sitl_px4_flight_stack
-    roslaunch innopolis_vtol_dynamics sitl.launch   \
-        logging_type:=quadcopter                    \
-        sitl_vehicle:=iris                          \
-        vehicle_params:=quadrotor                   \
-        mixer:=direct_mixer                         \
-        dynamics:=quadcopter                        \
-        run_sitl_flight_stack:="true"
 }
 
 
@@ -250,14 +218,10 @@ elif [ "$1" = "px4_v1_15_0_cyphal_octoplane_vtol" ]; then
     px4_v1_15_0_cyphal_octoplane_vtol
 elif [ "$1" = "cyphal_and_dronecan_inno_vtol" ]; then
     cyphal_and_dronecan_inno_vtol
-elif [ "$1" = "sitl_inno_vtol" ]; then
-    sitl_inno_vtol
-elif [ "$1" = "sitl_flight_goggles" ]; then
-    sitl_flight_goggles
-elif [ "$1" = "sitl_inno_vtol_with_flight_stack" ]; then
-    sitl_inno_vtol_with_flight_stack
-elif [ "$1" = "sitl_flight_goggles_with_flight_stack" ]; then
-    sitl_flight_goggles_with_flight_stack
+elif [ "$1" = "px4_v1_12_0_mavlink_quadplane_vtol" ]; then
+    px4_v1_12_0_mavlink_quadplane_vtol
+elif [ "$1" = "px4_v1_12_0_mavlink_quadcopter" ]; then
+    px4_v1_12_0_mavlink_quadcopter
 elif [ "$1" = "ros" ]; then
     setup_ros
 else
